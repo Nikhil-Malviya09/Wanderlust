31f8d518d1bbc2adecf536eef0927553c06cf766
Nikhil Malviya <nikhilmalviya094@gmail.com>
2025-12-21 18:21:18 +0530

    update show.ejs

--- utils/cloudConfig.js
+++ utils/cloudConfig.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'Wanderlust_DEV',
+    folder: 'Wanderlust_DEV', // Wanderlust_DEV
     allowedFormats: ["png","jpg", "jpeg"],
   },
 });
