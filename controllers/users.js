f560ff4e0e1671fa4dd70874032ab64a8be3dab7
Nikhil-Malviya09 <nikhilmalviya094@gmail.com>
2026-02-17 18:17:00 +0530

    Update users.js
    add features

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signupUser = async (req, res) => {
             if (err) {
                 return next(err);
             }
-            req.flash("success", "Welcome to Wanderlust!");
+            req.flash("success", "Welcome to Wandero!");
             res.redirect("/listings");
         })
     } catch (e) {
@@ -28,7 +28,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-    req.flash("success", "Welcome back to Wanderlust!");
+    req.flash("success", "Welcome back to Wandero!");
     let redirectUrl = res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
 };
