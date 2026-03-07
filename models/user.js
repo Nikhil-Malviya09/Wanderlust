eb15f4d6d8b2c17a350ac0d31a2de591cd224e2a
Nikhil-Malviya09 <nikhilmalviya094@gmail.com>
2026-03-07 18:42:56 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -5,7 +5,21 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
+    },
+
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+
+    otp: {
+        type: String
+    },
+
+    otpExpiry: {
+        type: Date
     }
 });
 
